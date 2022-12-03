c1a0f61156c3adbf290836d0864dce56dfc1d353
Mitchell Johnson <mjohnson@canva.com>
2022-12-03 11:44:17 +0000

    it's bad, but it solved day 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { day1, day2 } = require('./input.js');
+const { day1, day2, day3 } = require('./input.js');
 const divide = () => console.log('///////////////////////////////////////');
 
 const runDay1 = () => {
   const chunks = day1.split('\n');
-  
+
   let totals = [];
   let current_total = 0;
   while (chunks.length > 0) {
@@ -17,16 +17,16 @@ const runDay1 = () => {
     }
   }
   console.log('Day 1:', Math.max(...totals))
-  
+
   const tier = [...totals];
-  
+
   const max_1 = Math.max(...tier);
   tier.splice(tier.indexOf(max_1), 1);
   const max_2 = Math.max(...tier);
   tier.splice(tier.indexOf(max_2), 1);
   const max_3 = Math.max(...tier);
   tier.splice(tier.indexOf(max_3), 1);
-  
+
   console.log('Day 1 pt2:', max_1 + max_2 + max_3);
 }
 const runDay2 = () => {
@@ -35,45 +35,45 @@ const runDay2 = () => {
       return line.split(' ')
     });
   }
-  
+
   const rounds = parseDay2(day2);
-  
+
   const ROCK = 'R';
   const PAPER = 'P';
   const SCISSORS = 'S';
-  
+
   const ELF_ROCK = 'A';
   const ELF_PAPER = 'B';
   const ELF_SCISSORS = 'C';
-  
+
   const HUMAN_ROCK = 'X';
   const HUMAN_PAPER = 'Y';
   const HUMAN_SCISSORS = 'Z';
-  
+
   const valueMap = {
     [ROCK]: 1,
     [PAPER]: 2,
     [SCISSORS]: 3,
   }
-  
+
   const winPlayMap = {
     [ROCK]: PAPER,
     [PAPER]: SCISSORS,
     [SCISSORS]: ROCK,
   }
-  
+
   const losePlayMap = {
     [ROCK]: SCISSORS,
     [PAPER]: ROCK,
     [SCISSORS]: PAPER,
   }
-  
+
   const drawPlayMap = {
     [ROCK]: ROCK,
     [PAPER]: PAPER,
     [SCISSORS]: SCISSORS,
   }
-  
+
   const normalizeMap = {
     [ELF_ROCK]: ROCK,
     [HUMAN_ROCK]: ROCK,
@@ -82,25 +82,25 @@ const runDay2 = () => {
     [ELF_SCISSORS]: SCISSORS,
     [HUMAN_SCISSORS]: SCISSORS,
   }
-  
+
   const resultCodeMap = {
     X: 'LOSE',
     Y: 'DRAW',
     Z: 'WIN',
   }
-  
+
   const normalize = (play) => normalizeMap[play];
-  
+
   const getWinningPlay = (play) => winPlayMap[play];
   const getDrawingPlay = (play) => drawPlayMap[play];
   const getLosingPlay = (play) => losePlayMap[play];
-  
+
   const battlePointsFrom = (adversary, you) => {
     if (getWinningPlay(adversary) == you) return [0, 6]; // you won
     if (getDrawingPlay(adversary) == you) return [3, 3]; // you drew
     if (getLosingPlay(adversary) == you) return [6, 0];  // you lost
   }
-  
+
   const score = (round) => {
     const battle_points = battlePointsFrom(round.elf, round.human);
     return {
@@ -108,15 +108,15 @@ const runDay2 = () => {
       human: valueMap[round.human] + battle_points[1],
     }
   }
-  
+
   const getDesiredPlay = (play, desired_result_code) => {
-    switch(resultCodeMap[desired_result_code]) {
+    switch (resultCodeMap[desired_result_code]) {
       case 'WIN': return getWinningPlay(play);
       case 'LOSE': return getLosingPlay(play);
       case 'DRAW': return getDrawingPlay(play);
     }
   }
-  
+
   const result = (version) => rounds.reduce((scores, row) => {
     const row_result = score(version ? {
       elf: normalize(row[0]),
@@ -133,10 +133,56 @@ const runDay2 = () => {
     elf: 0,
     human: 0,
   });
-  
+
   console.log("Day 2:", result(true).human);
   console.log("Day 2 pt2:", result(false).human);
 }
+const runDay3 = () => {
+  const input = day3.split('\n').filter(x => x.length > 0);
 
-runDay1();
-runDay2();
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabetUppercase = alphabet.toLocaleUpperCase();
+  const valueLookup = alphabet + alphabetUppercase;
+
+  const getValueOfChar = (c) => valueLookup.indexOf(c) + 1;
+  
+  const getCharInBothHalves = (line) => {
+    const firstHalf = line.substring(0, line.length / 2);
+    const secondHalf = line.substring(line.length / 2);
+    const intersection = firstHalf.split('').filter(x => {
+      return secondHalf.indexOf(x) > -1;
+    })[0];
+    return intersection;
+  }
+
+  const sum_of_values = input
+    .map(x => getValueOfChar(getCharInBothHalves(x)))
+    .reduce((a, b) => a + b, 0);
+
+  console.log(sum_of_values);
+
+  const badges = [];
+  let current_bundle = [];
+  for (let j = 0; j < input.length; ++j) {
+    current_bundle.push(input[j]);
+    if (current_bundle.length === 3) {
+      const intersection = current_bundle.join('').split('').filter(x => {
+        return current_bundle[0].indexOf(x) > -1
+          && current_bundle[1].indexOf(x) > -1
+          && current_bundle[2].indexOf(x) > -1;
+      })[0];
+      badges.push(intersection);
+      current_bundle = []
+    }
+  }
+
+  const sum_of_badges = badges
+    .map(x => getValueOfChar(x))
+    .reduce((a, b) => a + b, 0);
+
+  console.log(sum_of_badges)
+}
+
+// runDay1();
+// runDay2();
+runDay3();
